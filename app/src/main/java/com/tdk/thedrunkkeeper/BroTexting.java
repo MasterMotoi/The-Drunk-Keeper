12c9157e2ef866f260b9d2c036bf1c2ace56c226
MasterMotoi <fabien.plastina@viacesi.fr>
2018-01-03 12:47:04 +0100

    Send a mesage with the default sms app

--- app/src/main/java/com/tdk/thedrunkkeeper/BroTexting.java
+++ app/src/main/java/com/tdk/thedrunkkeeper/BroTexting.java
@@ -1,8 +1,10 @@
 package com.tdk.thedrunkkeeper;
 
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,15 +17,22 @@ public class BroTexting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bro_texting);
 
-        EditText name = (EditText) findViewById(R.id.bro_texting_name_in);
+        final EditText name = (EditText) findViewById(R.id.bro_texting_name_in);
         Button sendButton = (Button) findViewById(R.id.bro_texting_send_btn);
-        EditText number = (EditText) findViewById(R.id.bro_texting_phone_numb_in);
+        final EditText number = (EditText) findViewById(R.id.bro_texting_phone_numb_in);
         final TextView intro = (TextView) findViewById(R.id.bro_texting_intro_txt);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intro.setText("Hello World");
+                String num = number.getText().toString();
+                String nom = name.getText().toString();
+                Intent sms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+num));
+                sms.putExtra("sms_body", "Salut "+nom+" che passa");
+                startActivity(sms);
+                //SmsManager manager = SmsManager.getDefault();
+                //manager .sendTextMessage(num, null, "Salut "+nom+" c'est la voix qui te parle", null, null);
+
             }
         });
 
